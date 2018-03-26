61f857b138b811034b3a12b9b6b921513565ebde
euxn23 <euxn23@gmail.com>
2018-03-26 16:57:06 +0900

    Query selector all

--- src/react-webpacker.js
+++ src/react-webpacker.js
@@ -3,20 +3,22 @@ import ReactDOM from 'react-dom'
 
 export default {
   render() {
-    document.querySelectorAll('div[data-component-name]').forEach(dom => {
-      const componentName = dom.getAttribute('data-component-name')
-      const component = window[componentName]
-      if (!component)
-        return console.error(
-          `Component ${componentName} is not defined in window scope`
+    Array.from(document.querySelectorAll('div[data-component-name]')).forEach(
+      dom => {
+        const componentName = dom.getAttribute('data-component-name')
+        const component = window[componentName]
+        if (!component)
+          return console.error(
+            `Component ${componentName} is not defined in window scope`
+          )
+        ReactDOM.render(
+          React.createElement(
+            component,
+            JSON.parse(dom.getAttribute('data-props') || {})
+          ),
+          dom
         )
-      ReactDOM.render(
-        React.createElement(
-          component,
-          JSON.parse(dom.getAttribute('data-props') || {})
-        ),
-        dom
-      )
-    })
+      }
+    )
   }
 }
