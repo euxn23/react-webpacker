c096ac2ad176b8b7fa59742b26c807b1f1166a92
euxn23 <euxn23@gmail.com>
2018-06-15 18:35:27 +0900

    Provide d-ts

--- src/react-webpacker.js
+++ src/react-webpacker.js
@@ -1,24 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
-export default {
-  render(components = window) {
-    [...document.querySelectorAll('div[data-component-name]')].forEach(
-      dom => {
-        const componentName = dom.getAttribute('data-component-name')
-        const component = components[componentName]
-        if (!component)
-          return console.error(
-            `Component ${componentName} is not defined`
-          )
-        ReactDOM.render(
+export function render(components: { [key: string]: any } = window): void {
+  Array.from(document.querySelectorAll('div[data-component-name]')).forEach(
+    dom => {
+      const componentName = dom.getAttribute('data-component-name')
+      if (!componentName)
+        return console.error(`Component name is not defined in target DOM`)
+      const component = components[componentName]
+      if (!component)
+        return ReactDOM.render(
           React.createElement(
             component,
-            JSON.parse(dom.getAttribute('data-props') || {})
+            JSON.parse(dom.getAttribute('data-props') || '{}')
           ),
           dom
         )
-      }
-    )
-  }
+    }
+  )
 }
+
+const ReactWebpacker = { render }
+export default ReactWebpacker
