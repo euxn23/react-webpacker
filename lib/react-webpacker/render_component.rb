9e369fe5d64fdfbf58dd774ec28e1cc749157603
Chen Jun <chen.jun@livesense.co.jp>
2018-07-06 19:03:04 +0900

    Add support for multi dimensional props

--- lib/react-webpacker/render_component.rb
+++ lib/react-webpacker/render_component.rb
@@ -4,7 +4,7 @@ module ReactWebpacker
   def render_component(component_name, props = {})
     html_options = {
       'data-component-name' => component_name,
-      'data-props' => props.map { |k, v| [k.to_s.camelize(:lower), v] }.to_h.to_json
+      'data-props' => props.deep_transform_keys { |key| key.to_s.camelize(:lower) }.to_json
     }
     content_tag('div', '', html_options).html_safe
   end
